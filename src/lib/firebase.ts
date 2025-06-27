a9f8896dc3da21decda9233c2fcf7da33adc53f4
SHUBHAM UKEY <142341071+shubham-ukey@users.noreply.github.com>
2025-06-27 16:53:27 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-    const firebaseConfig = {
-    apiKey: "AIzaSyBiWa_l08h4Mlz69pCKNkquSTG3s92Jb-Q",
-    authDomain: "worrier-app.firebaseapp.com",
-    projectId: "worrier-app",
-    storageBucket: "worrier-app.firebasestorage.app",
-    messagingSenderId: "1088524659459",
-    appId: "1:1088524659459:web:be9dea59e866c1eb2ad736",
-  };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVMWdEn3QkdQ_c1cXpAkoS257D8Wz5-NM",
+  authDomain: "shuddha-32217.firebaseapp.com", 
+  projectId: "shuddha-32217",
+  storageBucket: "shuddha-32217.firebasestorage.app",
+  messagingSenderId: "809995789251",
+  appId: "1:809995789251:web:18520da28b9d0a2f999376"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
